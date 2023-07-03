adfff1842cb7dba4b89147c30a27ef4b5e99c7f8
Tuan.Nguyen3 <tuan.nguyen3@trustingsocial.com>
2023-07-03 09:35:16 +0700

    [add] conf and some util

--- processor/src/main/java/com/spark/etl/processor/app/ETLProcessAbstract.java
+++ processor/src/main/java/com/spark/etl/processor/app/ETLProcessAbstract.java
@@ -1,10 +1,15 @@
 package com.spark.etl.processor.app;
 
 import com.spark.etl.processor.entity.ETLPipelineConfig;
+import org.apache.log4j.Logger;
 import org.apache.spark.sql.SparkSession;
+import scala.collection.immutable.Map;
 
 public abstract class ETLProcessAbstract {
+    private static final Logger LOGGER = Logger.getLogger(ETLProcessAbstract.class);
     public static void run(SparkSession sparkSession, ETLPipelineConfig etlConfig) {
-        System.out.println("Hello World!");
+        Map<String, String> conf = sparkSession.conf().getAll();
+        LOGGER.info("Spark Configuration: " + conf);
+        sparkSession.close();
     }
 }
--- processor/src/main/java/com/spark/etl/processor/entity/ETLPipelineConfig.java
+++ processor/src/main/java/com/spark/etl/processor/entity/ETLPipelineConfig.java
@@ -1,5 +1,6 @@
 package com.spark.etl.processor.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.spark.etl.processor.app.ETLProcessor;
 import org.apache.log4j.Logger;
 import org.yaml.snakeyaml.Yaml;
@@ -9,8 +10,11 @@ import java.util.Map;
 
 public class ETLPipelineConfig implements Serializable {
     private static final Logger LOGGER = Logger.getLogger(ETLProcessor.class);
+    @JsonProperty("version")
     String version;
+    @JsonProperty("job")
     ETLJobConfig job;
+
     public ETLPipelineConfig loadData(String fileConf) throws FileNotFoundException {
         Yaml yaml = new Yaml();
         InputStream inputStream = new FileInputStream(new File(fileConf));
