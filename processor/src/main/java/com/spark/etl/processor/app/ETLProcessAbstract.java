75fb7c4e3f8822ffab3d3fdcc46c92c242a899be
Tuan.Nguyen3 <tuan.nguyen3@trustingsocial.com>
2023-07-04 15:34:04 +0700

    [add] new logger

--- processor/src/main/java/com/spark/etl/processor/app/ETLProcessAbstract.java
+++ processor/src/main/java/com/spark/etl/processor/app/ETLProcessAbstract.java
@@ -1,15 +1,17 @@
 package com.spark.etl.processor.app;
 
 import com.spark.etl.processor.entity.ETLPipelineConfig;
-import org.apache.log4j.Logger;
 import org.apache.spark.sql.SparkSession;
+import org.slf4j.Logger;
 import scala.collection.immutable.Map;
 
+import org.slf4j.LoggerFactory;
+
 public abstract class ETLProcessAbstract {
-    private static final Logger LOGGER = Logger.getLogger(ETLProcessAbstract.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ETLProcessAbstract.class);
     public static void run(SparkSession sparkSession, ETLPipelineConfig etlConfig) {
         Map<String, String> conf = sparkSession.conf().getAll();
-        LOGGER.info("Spark Configuration: " + conf);
+        LOGGER.info("Spark Configuration: {} ",conf);
         sparkSession.close();
     }
 }
